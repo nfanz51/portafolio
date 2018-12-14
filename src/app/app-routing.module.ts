1735588d557a0a704e313efa9a6e0f1d44463453
Naren Fanz <nfanz51@gmail.com>
2018-12-13 22:32:14 -0500

    fin seccion 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 const app_routes: Routes = [
     { path: '', component: PortafolioComponent, pathMatch: 'full' },
     { path: 'about', component: AboutComponent, pathMatch: 'full' },
     { path: 'item/:id', component: ItemComponent, pathMatch: 'full' },
+    { path: 'search/:termino', component: SearchComponent, pathMatch: 'full' },
     { path: '**', pathMatch: 'full', redirectTo: '' }
     ];
 
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -10,6 +10,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
 
   constructor( private http: HttpClient ) {
@@ -21,12 +22,17 @@ export class ProductosService {
 
   private cargarProductos() {
 
+    return new Promise( ( resolve, reject) => {
+
     this.http.get('https://angular-html-14459.firebaseio.com/productos_idx.json')
         .subscribe( (resp: Producto[]) => {
           this.productos = resp;
           this.cargando = false;
-
+          resolve();
         });
+    });
+
+
 
 
   }
@@ -34,4 +40,38 @@ export class ProductosService {
   getProducto( id: string ) {
     return this.http.get(`https://angular-html-14459.firebaseio.com/productos/${ id }.json`);
   }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar filtro
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+  }
+
 }
